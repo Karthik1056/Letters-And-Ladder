a9f2e4f331dff567a3dce033839891c36bea5db6
Hemanth Gowda K R <hemanth2742004@gmail.com>
2025-10-14 19:13:03 +0530

    (auth) updated

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}} />
+      <Stack.Screen name="welcome" options={{headerShown: false}} />
       <Stack.Screen name="sign-in" options={{headerShown: false}} />
       <Stack.Screen name="sign-up" options={{headerShown: false}} />
 
